4b39779384a2791ad40cc76af5ba441d8d238409
Luke Kasson <luke.kasson@gmail.com>
2024-05-16 22:17:24 -0400

    clean up and add todo for paolo

--- main.py
+++ main.py
@@ -1,43 +1,21 @@
+import requests
 
-class StockController:
-    async def GetStock(self):
-        client = "HttpClient()"
-        symbol = "imb"
-        url = f"http://www.aphpavantage.co/query?function=global_qoutes={symbol}&apikey=demo"
-        httpResponse = await client.GetAsync(url)
-        response = await httpResponse.Content.ReadFromJsonAsync[StockData]()
-        payload = Payload()
-        payload.shouldbuy = True
-        payload.stocksymbol = response.globalquote._01symbol
+def GetStock(stocksymbol):
+    apikKey = "I2FIH3DMPX4A1GDM"
+    url = f"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={stocksymbol}&apikey={apikKey}"
+    httpHesponse = requests.get(url)
 
-        payload.stockdata.high = response.globalquote._03high
-        payload.stockdata.low = response.globalquote._04low
-        payload.stockdata.price = response.globalquote._05price
-        return Ok(response)
+    json = httpHesponse.json()
 
-class Payload:
-    def __init__(self):
-        self.shouldbuy = None
-        self.stocksymbol = None
-        self.stockdata = None
+    # TODO - create a dictiory from the json response and print it out. 
+    # Things we need 
+    # stocksymbol
+    # shouldbuy - if the price is higher than the close price
+    # price 
+    # high 
+    # low
 
-class GlobalQuote:
-    def __init__(self):
-        self._01symbol = None
-        self._02open = None
-        self._03high = None
-        self._04low = None
-        self._05price = None
-        self._06volume = None
-        self._07latesttradingday = None
-        self._08previousclose = None
-        self._09change = None
-        self._10changepercent = None
 
-class StockData:
-    def __init__(self):
-        self.globalquote = GlobalQuote()
 
-if __name__ == '__main__':
-    controller = StockController()
-    controller.GetStock()
+
+GetStock("ibm")
