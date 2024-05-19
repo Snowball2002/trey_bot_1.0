43257923c1528677c6d8ad0c9100c596e1b03cf0
Paolo-laur <124380575+Snowball2002@users.noreply.github.com>
2024-05-19 16:30:18 -0400

    getting an error on the = for the dictionary its saying Expected expression to the right of "="Pylance would you know why
    getting an error on the = for the dictionary its saying Expected expression to the right of "="Pylance would you know why this is preventing the code from printing

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import requests
 def GetStock(stocksymbol):
     apikKey = "I2FIH3DMPX4A1GDM"
     url = f"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={stocksymbol}&apikey={apikKey}"
-    httpHesponse = requests.get(url)
+    httpresponse = requests.get(url)
 
-    json = httpHesponse.json()
+    json = httpresponse.json()
 
     # TODO - create a dictiory from the json response and print it out. 
     # Things we need 
@@ -13,8 +13,20 @@ def GetStock(stocksymbol):
     # shouldbuy - if the price is higher than the close price
     # price 
     # high 
-    # low
-
+    # low 
+    #getting a simple error preventing it from printing 
+    # on line 19 @ = 
+info_dictionary =
+{ "stocksymbol": stocksymbol,
+        "shouldbuy": shouldbuy,
+        "price": price,
+        "high": high,
+        "low": low
+}
+ 
+ 
+ 
+print (info_dictionary)
 
 
 
