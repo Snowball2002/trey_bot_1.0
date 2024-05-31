4a466318c59c5cd4954bf210a100e0d582ef530a
Luke Kasson <luke.kasson@gmail.com>
2024-05-30 21:31:13 -0400

    fix it to work

--- main.py
+++ main.py
@@ -7,30 +7,20 @@ def GetStock(stocksymbol):
 
     json = httpresponse.json()
 
-    # TODO - create a dictiory from the json response and print it out. 
-    # Things we need 
-    # stocksymbol
-    # shouldbuy - if the price is higher than the close price
-    # price 
-    # high 
-    # low 
-    #getting a simple error preventing it from printing 
-    # on line 19 @ = 
-stocksymbol = json_data ['global quote'] ['01. symbol']
-price = float(json_data['Global Quote']['05. price'])
-previous_close = float (json_data['global quote']['0.8 previous close'])
-low = float(json_data['Global Quote']['04. low'])
-high = float(json_data['Global Quote']['03. high'])
-info_dictionary = { "stocksymbol": stocksymbol,
-        "shouldbuy": shouldbuy,
-        "price": price,
-        "high": high,
-        "low": low
-}
- 
- 
- 
-print (info_dictionary)
+    stocksymbol = json ['Global Quote'] ['01. symbol']
+    price = json['Global Quote']['05. price']
+    previous_close = json['Global Quote']['08. previous close']
+    low = json['Global Quote']['04. low']
+    high = json['Global Quote']['03. high']
+    shouldbuy = price > previous_close
+    info_dictionary = { 
+            "stocksymbol": stocksymbol,
+            "shouldbuy": shouldbuy,
+            "price": price,
+            "high": high,
+            "low": low
+    }
+    print (info_dictionary)
 
 
 
