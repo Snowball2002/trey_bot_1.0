3d82bd8cd1906d3264a28f15a90183f720457998
Paolo-laur <124380575+Snowball2002@users.noreply.github.com>
2024-05-30 21:36:14 -0400

    update
    trey

--- main.py
+++ main.py
@@ -1,37 +1,48 @@
-import requests
+def get_stock_info(json_data):
 
-def GetStock(stocksymbol):
-    apikKey = "I2FIH3DMPX4A1GDM"
-    url = f"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={stocksymbol}&apikey={apikKey}"
-    httpresponse = requests.get(url)
+    stocksymbol = json_data['Global Quote']['01. symbol']
+    price = float(json_data['Global Quote']['05. price'])
+    previous_close = float(json_data['Global Quote']['08. previous close'])
+    high = float(json_data['Global Quote']['03. high'])
+    low = float(json_data['Global Quote']['04. low'])
 
-    json = httpresponse.json()
 
-    # TODO - create a dictiory from the json response and print it out. 
-    # Things we need 
-    # stocksymbol
-    # shouldbuy - if the price is higher than the close price
-    # price 
-    # high 
-    # low 
-    #getting a simple error preventing it from printing 
-    # on line 19 @ = 
-stocksymbol = json_data ['global quote'] ['01. symbol']
-price = float(json_data['Global Quote']['05. price'])
-previous_close = float (json_data['global quote']['0.8 previous close'])
-low = float(json_data['Global Quote']['04. low'])
-high = float(json_data['Global Quote']['03. high'])
-info_dictionary = { "stocksymbol": stocksymbol,
+    shouldbuy = price > previous_close
+
+
+    info_dictionary = {
+        "stocksymbol": stocksymbol,
         "shouldbuy": shouldbuy,
         "price": price,
         "high": high,
         "low": low
+    }
+
+
+    print("Stock Symbol:", stocksymbol)
+    print("Should Buy:", shouldbuy)
+    print("Current Price:", price)
+    print("High Price:", high)
+    print("Low Price:", low)
+
+    return info_dictionary
+
+
+
+json_data = {
+    "Global Quote": {
+        "01. symbol": "IBM",
+        "02. open": "140.0000",
+        "03. high": "145.0000",
+        "04. low": "139.5000",
+        "05. price": "143.0000",
+        "06. volume": "3000000",
+        "07. latest trading day": "2024-05-22",
+        "08. previous close": "142.0000",
+        "09. change": "1.0000",
+        "10. change percent": "0.7042%"
+    }
 }
- 
- 
- 
-print (info_dictionary)
 
-
-
-GetStock("ibm")
+stock_info = get_stock_info(json_data)
+print(stock_info)
